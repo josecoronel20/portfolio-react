03f3ec8a8bc63b9f6ebf40d54be8b1007cea0a72
josecoronel20 <josecoronel20@outlook.com>
2023-10-07 20:30:10 -0300

    corrigiendo ruta del nav

--- src/components/navbarComponent/NavBar.js
+++ src/components/navbarComponent/NavBar.js
@@ -9,7 +9,7 @@ import { Icon } from "@iconify/react";
 const NavBar = () => {
     return(
         <div className="flex w-full bg-purple-900 justify-evenly p-2 fixed bottom-0 h-20">
-            <Button svg={<Icon icon="material-symbols:developer-mode-outline" color="rgba(237, 233, 254, 0.8)" width="35" />} name={"Proyectos"} Url={"/"}/>
+            <Button svg={<Icon icon="material-symbols:developer-mode-outline" color="rgba(237, 233, 254, 0.8)" width="35" />} name={"Proyectos"} url={"/"}/>
             <Button svg={<Icon icon="mdi:react" color="rgba(237, 233, 254, 0.8)" width="35" />} name={"Tecnologias"} url={"/technologies"}/>
             <Button svg={<Icon icon="pepicons-pop:cv-circle-filled" color="rgba(237, 233, 254, 0.8)" width="35" />} name={"CV"} url="nada"/>
             <Button svg={<Icon icon="ant-design:message-outlined" color="rgba(237, 233, 254, 0.8)" width="35" />} name={"Contactame"} url={"/contactMe"}/>
